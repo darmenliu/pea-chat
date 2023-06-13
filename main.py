7b83059ec054ced20647552aa848a110ee59fad8
yliu <yongqiang.liu@nokia-sbell.com>
2023-06-13 08:30:43 +0800

    Add main function as a for loop

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # TODO main function
 
 def main():
+    # TODO main function body
     pass
+
+# Run main as a for loop, it will run forever
+if __name__ == "__main__":
+    # Run main as a for loop, it will run forever
+    while True:
+        # Run main
+        main()
+        # Sleep 1 second
+        time.sleep(5)
