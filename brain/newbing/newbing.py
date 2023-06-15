0da973ef763c60f0112c4eac9b4ef0de33851a86
yliu <yongqiang.liu@nokia-sbell.com>
2023-06-15 08:50:30 +0800

    Add draft chatbot for newbing

--- brain/newbing/newbing.py
+++ brain/newbing/newbing.py
@@ -1,1 +1,23 @@
-# TODO This is backend of the newbing chatbot
+import asyncio, json
+from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
+from config import access_token_for_newbing
+
+class NewbingChatbot:
+    def __init__(self):
+        self.bot = Chatbot.create(access_token_for_newbing)
+
+    async def ask(self, prompt):
+        response = await self.bot.ask(prompt="Hello world", conversation_style=ConversationStyle.creative, simplify_response=True)
+        print(json.dumps(response, indent=2)) # Returns
+        """
+        {
+            "text": str
+            "author": str
+            "sources": list[dict]
+            "sources_text": str
+            "suggestions": list[str]
+            "messages_left": int
+        }
+        """
+        await self.bot.close()
+        return response["text"]
--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 # Access token for OpenAI chatbot, please configure it in config.py
 access_token_for_openai=""
 
+# Access token for newbing chat
+access_token_for_newbing=""
+
 # The http server will be run on port 5000 by default, you can change it in config.py
 http_server_port=5000
 
